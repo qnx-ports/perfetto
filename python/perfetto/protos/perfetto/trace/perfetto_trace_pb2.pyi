978dbb0b025f1bceb5f728b23e488efa6696d6b7
YH LIN <yhli@google.com>
2025-10-02 20:19:17 +0800

    trace_processor: support f2fs_write_checkpoint
    Add f2fs_write_checkpoint ftrace to slice for showing it on ui.
    
    Bug: 442974909

--- python/perfetto/protos/perfetto/trace/perfetto_trace_pb2.pyi
+++ python/perfetto/protos/perfetto/trace/perfetto_trace_pb2.pyi
@@ -7110,16 +7110,18 @@ class F2fsWriteBeginFtraceEvent(_message.Message):
     def __init__(self, dev: _Optional[int] = ..., ino: _Optional[int] = ..., pos: _Optional[int] = ..., len: _Optional[int] = ..., flags: _Optional[int] = ...) -> None: ...
 
 class F2fsWriteCheckpointFtraceEvent(_message.Message):
-    __slots__ = ["dev", "is_umount", "msg", "reason"]
+    __slots__ = ["dest_msg", "dev", "is_umount", "msg", "reason"]
+    DEST_MSG_FIELD_NUMBER: _ClassVar[int]
     DEV_FIELD_NUMBER: _ClassVar[int]
     IS_UMOUNT_FIELD_NUMBER: _ClassVar[int]
     MSG_FIELD_NUMBER: _ClassVar[int]
     REASON_FIELD_NUMBER: _ClassVar[int]
+    dest_msg: str
     dev: int
     is_umount: int
     msg: str
     reason: int
-    def __init__(self, dev: _Optional[int] = ..., is_umount: _Optional[int] = ..., msg: _Optional[str] = ..., reason: _Optional[int] = ...) -> None: ...
+    def __init__(self, dev: _Optional[int] = ..., is_umount: _Optional[int] = ..., msg: _Optional[str] = ..., reason: _Optional[int] = ..., dest_msg: _Optional[str] = ...) -> None: ...
 
 class F2fsWriteEndFtraceEvent(_message.Message):
     __slots__ = ["copied", "dev", "ino", "len", "pos"]
